24043ac463f50a3c2d29b1cb891098d69067a474
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-07-05 00:29:47 -0300

    exception léxica especifica

---
+++ analisadorlexico/src/isilanguage/exceptions/IsiLexicalException.java
@@ -0,0 +1,8 @@
+package analisadorlexico.src.isilanguage.exceptions;
+
+public class IsiLexicalException extends RuntimeException {
+    public IsiLexicalException(String message) {
+        super(message);
+    }
+}
+
