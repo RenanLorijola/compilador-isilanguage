bff7d44af18e723dcb1133d7c633a9a7fe4cd5bc
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-07-23 23:33:38 -0300

    Erro de sintaxe

---
+++ analisadorlexico/src/isilanguage/exceptions/IsiSyntaxException.java
@@ -0,0 +1,8 @@
+package analisadorlexico.src.isilanguage.exceptions;
+
+public class IsiSyntaxException extends RuntimeException {
+    public IsiSyntaxException(String msg) {
+        super(msg);
+    }
+
+}
