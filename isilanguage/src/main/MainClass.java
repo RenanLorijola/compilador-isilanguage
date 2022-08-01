2e8eee18a97c910f36e5245cbea0a4da218d92a5
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2022-08-01 19:29:12 -0300

    Adicionando IsiSemanticException a MainClass

---
+++ isilanguage/src/main/MainClass.java
@@ -0,0 +1,34 @@
+package isilanguage.src.main;
+
+
+import isilanguage.src.exceptions.IsiSemanticException;
+import isilanguage.src.parser.IsiLangLexer;
+import isilanguage.src.parser.IsiLangParser;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+
+public class MainClass {
+    public static void main(String[] args) {
+        try {
+            IsiLangLexer lexer;
+            IsiLangParser parser;
+
+            lexer = new IsiLangLexer(CharStreams.fromFileName("test.isi"));
+            CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+            parser = new IsiLangParser(tokenStream);
+
+            parser.program();
+            System.out.println("Success!");
+
+        }
+        catch (IsiSemanticException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        catch (Exception err) {
+            System.err.println("Generic Error: " + err.getMessage());
+        }
+
+    }
+}
+
